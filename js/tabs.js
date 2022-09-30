0f8d1792ae2ff1eac130fdc9464fade0fd320a3b
Peresmeshnica <polino4ka797@mail.ru>
2022-10-01 06:20:38 +0700

    Не разобралась с stretched-link, но искренне пыталась

--- js/tabs.js
+++ js/tabs.js
@@ -1,5 +1,2 @@
-const triggerEl = document.querySelector('#myTab a[href="#profile"]')
-bootstrap.Tab.getInstance(triggerEl).show() // Select tab by name
 
-const triggerFirstTabEl = document.querySelector('#myTab li:first-child a')
-bootstrap.Tab.getInstance(triggerFirstTabEl).show() // Select first tab
+
